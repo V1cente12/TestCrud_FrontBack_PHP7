1a1b13c828410bdf5bf69cf3a71f4ef2c9103d53
Vicente Calderon <77591317+V1cente12@users.noreply.github.com>
2024-08-02 23:47:23 -0400

    Actualizar formulario.php

--- formulario.php
+++ formulario.php
@@ -11,7 +11,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <body>
     <h1>AGREGAR NUEVO PRODUCTO</h1>
-    
+    //mandar por post registro de producto
     <form action="new_product.php" method="post">
     <div class="form-group">
         <label for="exampleInputEmail1">Nombre del producto</label>
